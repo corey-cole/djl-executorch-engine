81b2b3013cf509fba766291cb39cb6ea70668383
Corey Cole <3700879+corey-cole@users.noreply.github.com>
2026-06-30 19:59:48 -0700

    Prepare for eventual publish to Maven Central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     `java-library`
+    id("com.vanniktech.maven.publish") version "0.37.0"
 }
 
 group = "org.measly"
@@ -37,4 +38,39 @@ tasks.register<Test>("leakTest") {
     classpath = sourceSets["test"].runtimeClasspath
     useJUnitPlatform { includeTags("leak") }
     jvmArgs("-Xmx256m", "-XX:MaxDirectMemorySize=64m", "-XX:+HeapDumpOnOutOfMemoryError")
+}
+
+mavenPublishing {
+    // Automatically generates sources, javadocs, and handles GPG signing
+    publishToMavenCentral()
+    
+    // Automatically signs if GPG environment variables are present
+    signAllPublications()
+
+    coordinates("org.measly", "djl-executorch-engine", "0.1.0-SNAPSHOT")
+
+    pom {
+        name.set("DJL ExecuTorch Engine")
+        description.set("Enable use of ExecuTorch (PTE) models within DJL")
+        inceptionYear.set("2026")
+        url.set("https://github.com/corey-cole/djl-executorch-engine")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("corey-cole")
+                name.set("Corey Cole")
+                url.set("https://github.com/corey-cole")
+            }
+        }
+        scm {
+            url.set("https://github.com/corey-cole/djl-executorch-engine")
+            connection.set("scm:git:git://github.com/corey-cole/djl-executorch-engine.git")
+            developerConnection.set("scm:git:ssh://git@github.com/corey-cole/djl-executorch-engine.git")
+        }
+    }
 }
